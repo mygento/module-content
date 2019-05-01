b412fa3d8c0f7b8225c92d07bc1a794cd1928932
Nikita Tarasov <nikita@mygento.ru>
2019-05-01 21:01:38 +0300

    cms controller

---
+++ Block/Adminhtml/System/Config/CmsBlockExport.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Mygento\Content\Block\Adminhtml\System\Config;
+
+class CmsBlockExport extends \Mygento\Base\Block\Adminhtml\System\Config\AjaxButton
+{
+    protected $url = 'mygentocontent/export/cms';
+}
